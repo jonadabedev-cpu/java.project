213dbe5f0bebe2ab10840b2681c44c49e5f61300
jonadabedev-cpu <jonadabedev@gmail.com>
2026-04-22 17:29:52 -0300

    Initial commit

---
+++ src/main/java/santader/aplicacao/SantanderDevWook2006Application.java
@@ -0,0 +1,13 @@
+package santader.aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWook2006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWook2006Application.class, args);
+	}
+
+}
---
+++ src/test/java/santader/aplicacao/SantanderDevWook2006ApplicationTests.java
@@ -0,0 +1,13 @@
+package santader.aplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWook2006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
